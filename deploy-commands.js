76cbde0d5a32f3972ed43238a1d8a634444b9156
Tuur MISPELAERE <tuur.mispelaere@student.howest.be>
2026-01-17 10:20:50 +0100

    .

--- deploy-commands.js
+++ deploy-commands.js
@@ -17,7 +17,6 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
         await rest.put(
             Routes.applicationGuildCommands(
                 process.env.CLIENT_ID,
-                process.env.GUILD_ID
             ),
             { body: commands }
         );
